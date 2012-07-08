90f52cef9dd266f66d7a99048126d6d2b02f69f1
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2012-07-09 09:40:03 +1200

    Imported Debian patch 2.5.0-2~exp1

--- debian/check.sh
+++ debian/check.sh
@@ -11,23 +11,35 @@ cleanup(){
 trap "cleanup" 1 2 3 13 15
 
 CWD=$(pwd)
+cd $1/examples
 
-for dir in examples/*/serial ; do
-    cd $dir
-    for prog in $(ls .libs | fgrep -v .o | grep -v ^lt- | sed 's:.libs/::') ; do
-        echo -n "Checking $dir/$prog... "
-        ./$prog > $RESULTS
-        if test -e $prog.out ; then
-            DIFF=$(diff -u $prog.out $RESULTS)
-            if test -z "$DIFF" ; then
-                echo pass
+for dir in $(find . -type d -maxdepth 1 -mindepth 1) ; do
+    #for mode in "serial" "fcmix_serial" "parallel" "fcmix_parallel"  ; do
+    for mode in "serial" ; do
+	cd $dir/$mode
+	echo "tests in " $mode
+	for prog in $(find . -executable -type f) ; do
+            echo -n "Checking $prog... "
+            if [ "$mode" != "parallel" ]; then
+	       ./$prog > $RESULTS
+	    else
+	       OMPI_MCA_plm_rsh_agent=/bin/false ./$prog > $RESULTS
+	    fi
+
+            if test -e $2/examples/$dir/serial/$prog.out ; then
+        	DIFF=$(diff -u $2/examples/$dir/serial/$prog.out $RESULTS)
+        	if test -z "$DIFF" ; then
+                    echo success
+        	else
+                    echo fail
+                    echo "$DIFF"
+        	fi
             else
-                echo fail
-                echo "$DIFF"
+        	echo pass
             fi
-        else
-            echo pass
-        fi
+	done
+	cd $1/examples
     done
-    cd $CWD
 done
+
+cd $CWD
